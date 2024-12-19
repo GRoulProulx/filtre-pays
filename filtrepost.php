e8d0b4887febd601fb77c6453d66f11402aeca11
GRoulProulx <gproulx85@gmail.com>
2024-12-19 10:51:36 -0500

    Modif .php

--- filtrepost.php
+++ filtrepost.php
@@ -1,28 +1,26 @@
 <?php
-
-/* 
-plugin name : Filtre post
-author: Guillaume Rouleau Proulx
-description: Une extension qui permettra de filtrer nos articles
-
+/*
+plugin name: Filtre Post
+description: Une extension qui permettra de filtrer les articles de la page web.
+author: Guillaume Proulx
 */
 
 function charger_scripts_css(){
  
     $version_css = filemtime(plugin_dir_path(__FILE__). "/style.css");
-    $version_js = filemtime(plugin_dir_path(__FILE__) . "js/filtrepost.js");
+    $version_js = filemtime(plugin_dir_path(__FILE__) . "js/filtrePost.js");
  
     wp_enqueue_style(
-        "filtrepost",        
+        "filtrePost",        
         plugin_dir_url(__FILE__) . "/style.css",
-        array(),
+        [],
         $version_css
     ) ;  
  
     wp_enqueue_script(
-        "filtrepost",      
-        plugin_dir_url(__FILE__) . "/js/filtrepost.js",
-        array(),
+        "filtrePost",      
+        plugin_dir_url(__FILE__) . "/js/filtrePost.js",
+        [],
         $version_js,
         true
     )  ;
@@ -30,14 +28,18 @@ function charger_scripts_css(){
 add_action("wp_enqueue_scripts", "charger_scripts_css");
 
 function genere_boutons() {
-    $categories = get_categories();
-    $contenu = "";
-    foreach($categories as $elm) {
-        $nom = $elm->name;
-        $id = $elm->term_id;
-        $contenu .= "<button data-id='$id'>$nom</button>";
-    }
-    return "<div class='filtre__button'>$contenu</div>";
+$categories = get_categories(array('exclude' => array(10,14)));
+$content = "";
+
+foreach ($categories as $element) {
+$nom = $element->name;
+$id = $element->term_id;
+$content .= "<button data-id='$id'>$nom</button>";
+}
+return '<div class="filtre__bouton">' . $content . '</div>
+        <div class="contenu__restapi"></div>';
 }
 
-add_shortcode("extraire_cours", "genere_button");
+
+
+add_shortcode("extraire_categorie", "genere_boutons");
